89a127b286b000e7d4e7716342312253b756e735
chamaraBritanext <chamara@britanext.com>
2025-06-01 23:42:10 +0530

    update robots file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin", "/api"],
+      disallow: ["/admin"],
     },
     sitemap: [
       "https://tamilmedia.lk/sitemap.xml",
